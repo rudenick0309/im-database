34c699e3a39e5d39d656bc0e91d8fdc4ad6f193d
Lee Hoyong <igotoweb@gmail.com>
2020-03-24 18:11:55 +0900

    make ci runnable

--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 USE chat;
+/*
 CREATE TABLE messages (
-  /*
-      remove here and create your message table
-    */
+  //  remove here and create your message table
 );
+*/
 /* Create other tables and define schemas for them here! */
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
