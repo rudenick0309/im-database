1661634cdf8ed07482428d6c4fd876f9a18f4221
rudenick0309 <rudenick0309@gmail.com>
2020-05-07 10:51:10 +0900

    minimun

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,8 +9,7 @@ module.exports = {
 		get: async function (req, res) {
 			try {
 				let data = await models.messages.get();
-				const result = await res.send(data);
-				return result;
+				res.status(200).send(data);
 			} catch (err) {
 				res.send(err);
 			}
@@ -18,23 +17,40 @@ module.exports = {
 			// res.status(200).send(JSON.stringify(data));
 		}, // a function which handles a get request for all messages
 		post: async function (req, res) {
-			let data = await models.messages.post(req.body);
-			console.log("포스트 메세지", data);
-			res.status(200).send(JSON.stringify(data));
+			try {
+				let data = await models.messages.post(req.body);
+				res.status(201).send(data);
+			} catch (err) {
+				res.send(err);
+			}
+			// console.log("포스트 메세지", data);
+			// res.status(200).send(JSON.stringify(data));
 			// var userName = document.querySelector(".inputUser").value;
 		}, // a function which handles posting a message to the database
 	},
 
 	users: {
 		get: async function (req, res) {
-			let data = await models.messages.get();
-			console.log("겟 유저", data);
-			res.status(200).send(JSON.stringify(data));
+			try {
+				let data = await models.users.get();
+				const result = await res.send(data);
+				return result;
+			} catch (err) {
+				res.send(err);
+			}
+			// console.log("겟 유저", data);
+			// res.status(200).send(JSON.stringify(data));
 		}, // a function which handles a get request for all users
 		post: async function (req, res) {
-			let data = await models.messages.post(req.body);
-			console.log("포스트 유저", data);
-			res.status(200).send(JSON.stringify(data));
+			try {
+				let data = await models.users.post(req.body);
+				const result = await res.send(data);
+				return result;
+			} catch (err) {
+				res.send(err);
+			}
+			// console.log("포스트 유저", data);
+			// res.status(200).send(JSON.stringify(data));
 		}, // a function which handles posting a user to the database
 	},
 };
